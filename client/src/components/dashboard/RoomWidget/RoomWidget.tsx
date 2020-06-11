7a2d5f91f5b8d96735db6afbe54fe74bae653d72
Paul Linck <paul.linck@gmail.com>
2020-06-11 18:15:46 -0400

    Rooms

--- client/src/components/dashboard/RoomWidget/RoomWidget.tsx
+++ client/src/components/dashboard/RoomWidget/RoomWidget.tsx
@@ -10,18 +10,15 @@ const PlayerWidget = (props:any) => {
                 <Typography variant="h5" style={{ fontStyle: "italic" }}>
                     Rooms:
                 </Typography>
-                <Typography variant="h5" style={{ fontStyle: "italic" }}>
-                    Interlock:
-                </Typography>
                 <TextFieldCH publishSignalName="21" subscribeSignalName="21" />
                 <ButtonCH 
                     publishSignalName="21"
                     subscribeSignalName="21"
-                    >A-21
+                    >Family
                 </ButtonCH>
-                <ButtonCH publishSignalName="22" subscribeSignalName="22" >B-22</ButtonCH>
-                <ButtonCH publishSignalName="23" subscribeSignalName="23" >C-23</ButtonCH>
-                <ButtonCH publishSignalName="24" subscribeSignalName="24" >D-24</ButtonCH>
+                <ButtonCH publishSignalName="22" subscribeSignalName="22" >Theater</ButtonCH>
+                <ButtonCH publishSignalName="23" subscribeSignalName="23" >Dining</ButtonCH>
+                <ButtonCH publishSignalName="24" subscribeSignalName="24" >Boat Dock</ButtonCH>
             </CardContent>
         </Card>
     )
